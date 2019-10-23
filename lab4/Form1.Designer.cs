b5e82730cc04506b8191196876ecd97173a1aef7
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2019-10-23 14:07:51 +0300

    lab4

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Info = new System.Windows.Forms.TextBox();
+            this.memo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(726, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Info
+            // 
+            this.Info.Location = new System.Drawing.Point(47, 443);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(635, 22);
+            this.Info.TabIndex = 1;
+            // 
+            // memo
+            // 
+            this.memo.Location = new System.Drawing.Point(47, 27);
+            this.memo.Multiline = true;
+            this.memo.Name = "memo";
+            this.memo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.memo.Size = new System.Drawing.Size(803, 393);
+            this.memo.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 493);
+            this.Controls.Add(this.memo);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №4";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox Info;
+        private System.Windows.Forms.TextBox memo;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        //public TcpClient mail;
+
+        TcpClient mail = new TcpClient();
+        SslStream sslStream;
+        int bytes = -1;
+        byte[] buffer = new byte[2048];
+
+        public NetworkStream netStream;
+        public StreamReader streamReader;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sslStream.Write(Encoding.ASCII.GetBytes(Info.Text + "\r\n"));
+            memo.Text += Info.Text + "\r\n";
+            if ((Info.Text == "LIST")||(Info.Text.Contains("RETR ")))
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                string data= Encoding.UTF8.GetString(buffer, 0, bytes);
+                while (!data.Contains("\r\n.\r\n")&& (data!=".\r\n"))
+                {
+                    memo.Text += data;
+                    bytes = sslStream.Read(buffer, 0, buffer.Length);
+                    data = Encoding.UTF8.GetString(buffer, 0, bytes);
+                }
+                memo.Text += (Encoding.UTF8.GetString(buffer, 0, bytes));
+            }
+            else
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                memo.Text += (Encoding.UTF8.GetString(buffer, 0, bytes));
+            }
+
+            Info.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            mail.Connect("pop.mail.ru", 995);
+            sslStream = new SslStream(mail.GetStream());
+            sslStream.AuthenticateAsClient("pop.mail.ru");
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            memo.Text += Encoding.ASCII.GetString(buffer, 0, bytes);
+        }
+    }
+}
